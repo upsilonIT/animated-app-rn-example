405d42e678204e96084877944d9c970de8b56144
angelinaromashko <anhelina.ramashka@upsilonit.com>
2022-05-08 19:20:18 +0300

    added reanimated library

--- App.tsx
+++ App.tsx
@@ -3,10 +3,10 @@ import {StatusBar, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar barStyle={'default'} />
-      </View>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle={'default'} />
+    </View>
   );
 };
 
